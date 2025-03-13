a53dddd349d2ead1bc11ee1f129888d97401d75e
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-13 18:26:55 +0100

    feat: stampata in pagina la lista email

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 
 const emailApi = 'https://flynn.boolean.careers/exercises/api/random/mail'
 
+const containerElement = document.querySelector('.container')
+console.log(containerElement)
 
 const emailList = [];
+let items='';
 for (let i = 0; i < 10; i++) {
     axios.get(emailApi)
     .then(response => {
         const email = response.data
         emailList.push(email)
         console.log(emailList[i].response)
+        items+=`<div><ul><li>${emailList[i].response}</li></ul></div>` 
+        containerElement.innerHTML = items;
         
+    }).catch(error => {
+        console.log(error)
     })
     
 }
